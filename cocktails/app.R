e62f80542206ee594b0c1cdfe048b2df6bf103cb
sanjnaduvvur <sanjna.duvvur@gmail.com>
2020-06-10 00:36:41 -0700

    Code commenting and clean up

--- cocktails/app.R
+++ cocktails/app.R
@@ -1,10 +1,11 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This Shiny application is built to make finding cocktail recipes easier
+# 
+# The user can prompt the application to choose a random cocktail recipe by selecting "Surpise Me!"
+# Alternately, the user can select ingredients they like and/or have available to them 
+# and the application will display possible recipes the user can create
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# TheCocktailDB API documentation: https://www.thecocktaildb.com/api.php
 #
 
 library(shiny)
@@ -12,8 +13,11 @@ library(tidyverse)
 library(jsonlite)
 library(lubridate)
 
+# Store all ingredients from API
 ingredients <- fromJSON("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")$drinks$strIngredient1
 
+# Function returns all ingredients needed for a particular drink
+# @param drink: parsed JSON of specific drink
 getIngredients <- function(drink){
   ingredients <- c(drink$drinks$strIngredient1,drink$drinks$strIngredient2,drink$drinks$strIngredient3,drink$drinks$strIngredient4,
                    drink$drinks$strIngredient5,drink$drinks$strIngredient6,drink$drinks$strIngredient7,drink$drinks$strIngredient8,
@@ -23,6 +27,8 @@ getIngredients <- function(drink){
   return(ingredients)
 }
 
+# Function returns all measurements of ingredients needed for a particular drink
+# @param drink: parsed JSON of specific drink
 getMeasures <- function(drink){
   measures <- c(drink$drinks$strMeasure1,drink$drinks$strMeasure2,drink$drinks$strMeasure3,drink$drinks$strMeasure4,drink$drinks$strMeasure5,
                 drink$drinks$strMeasure6,drink$drinks$strMeasure7,drink$drinks$strMeasure8,drink$drinks$strMeasure9,drink$drinks$strMeasure10,
@@ -31,12 +37,10 @@ getMeasures <- function(drink){
   return(measures)
 }
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-   # Application title
+
    titlePanel("Cocktail Maker"),
    
-   # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
        
@@ -48,10 +52,8 @@ ui <- fluidPage(
         width = 4
       ),
       
-      # Show a plot of the generated distribution
       mainPanel(
        
-        h3(textOutput("selected_ingredients")),
         h2(textOutput("name")),
         br(),
         uiOutput("drinkImage"),
@@ -69,26 +71,27 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
+  # Function returns list of cocktail ids that contain all ingredients selected by user
   drink_ids <- reactive({
     all_recipes <- fromJSON(str_glue("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={ingredient}", ingredient = URLencode(input$ingredients[1])))$drinks$idDrink
+    
     if(length(input$ingredients) > 1){
       for(val in input$ingredients) {
         recipes <- fromJSON(str_glue("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={ingredient}", ingredient = URLencode(val)))$drinks$idDrink
+        
+        # Compare cocktail ids and only keep drinks in common with existing list
         all_recipes <- intersect(all_recipes,recipes)
        }
     }
     all_recipes
   })
   
-  random_drink_details <- reactive({
-    random <- fromJSON("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-    random
-  })
-  
   counter <- reactiveVal(0)
   
+  # Called when user selects "Make a Cocktail" button
   observeEvent(input$manual_input, {
-    #clear existing UI
+    
+    # Clear existing UI
     output$name <- renderText({})
     output$drinkImage <- renderUI({})
     output$ingTitle <- renderText({})
@@ -97,22 +100,26 @@ server <- function(input, output, session) {
     output$instructions <- renderText({})
     output$nextButton <- renderUI({})
     
-    #initialize count
+    # Initialize counter
     counter(0)
     
+    # If no ingredients have been selected, show warning
     if(is.null(input$ingredients)){
       output$ingTitle <- renderText({
         print("Please enter at least one ingredient")
       })
-    } else{
+    } else{ # User has selected ingredients
       drink_ids <- drink_ids()
+      # If there are no cocktails with selected ingredients, show warning
       if(length(drink_ids) == 0){
         output$ingTitle <- renderText({
           print("There are no cocktail recipes with all of these ingredients")
         })
-      } else{
+      } else{ # One or more cocktail recipes exist
+        
         newCount <- counter() + 1
         counter(newCount)
+        
         drink <- fromJSON(str_glue("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}", id = drink_ids[newCount]))
         imgurl <- drink$drinks$strDrinkThumb
         ingredient <- getIngredients(drink)
@@ -143,6 +150,7 @@ server <- function(input, output, session) {
           drink$drinks$strInstructions
         })
         
+        # If there is more than 1 available recipe, provide next button
         if(length(drink_ids) > 1){
           output$nextButton <- renderUI({
             actionButton("next_drink", "next")
@@ -152,9 +160,12 @@ server <- function(input, output, session) {
     }
   })
   
+  # Called when user selects "next" button, iterates through all remaining cockatil recipes
   observeEvent(input$next_drink, {   
+    
     newCount <-counter() + 1
     counter(newCount)
+    
     drink_ids <- drink_ids()
     drink <- fromJSON(str_glue("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}", id = drink_ids[newCount]))
     imgurl <- drink$drinks$strDrinkThumb
@@ -191,14 +202,18 @@ server <- function(input, output, session) {
     }
   })
   
-  #display randomly chosen drink
+  # Called when user selects "Surprise Me!"; Displays randomly chosen cocktail
   observeEvent(input$random_input, {
+    
+    # Generate random cocktail
     random <- fromJSON("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    
     imgurl <- random$drinks$strDrinkThumb
     ingredient <- getIngredients(random)
     amount <- getMeasures(random)
     ingredients <- cbind(amount, ingredient)
     
+    # Clear existing UI
     output$nextButton <- renderUI({})
     
     output$name <- renderText({
